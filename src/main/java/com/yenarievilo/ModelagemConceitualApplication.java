2fad4fb3929ae0d17739532e104e029c6846547b
yenarievilo <jenifferloliveira@gmail.com>
2024-11-23 16:50:50 -0300

    Projeto Criado

---
+++ src/main/java/com/yenarievilo/ModelagemConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.yenarievilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemConceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yenarievilo/ModelagemConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yenarievilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
